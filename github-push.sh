edfaf9f3160a889ea5f0f12e3a5b2a597fe4d6b9
Hiroshi Kori <kori39@gmail.com>
2012-01-08 16:01:12 +0900

    assign repository name.

--- github-push.sh
+++ github-push.sh
@@ -4,17 +4,18 @@
 # http://help.github.com/create-a-repo/
 
 CMDNAME=`basename $0`
-if [ $# -ne 2 ]; then
-  echo "Usage: $CMDNAME file commit-message" 1>&2
+if [ $# -ne 3 ]; then
+  echo "Usage: $CMDNAME add-file commit-message repository-name" 1>&2
   exit 1
 fi
 
-echo "## filename      : $1"
-echo "## commit-message: $2"
+echo "## filename       : $1"
+echo "## commit-message : $2"
+echo "## repository-name: $2"
 
 git add $1
 git commit -m "$2"
-git remote add origin git@github.com:k-ori/sandbox.git
+git remote add origin git@github.com:k-ori/$3.git
 git push -u origin master
 
 exit 0
